0f3bc7b346d1aeb867dff424046452db467d5c60
JonasFiechter <jonasrfiechter@gmail.com>
2022-08-21 18:48:33 -0300

    update

--- src/features/populate_table.py
+++ src/features/populate_table.py
@@ -2,12 +2,22 @@ import pandas as pd
 
 def load_table(self):
     self.table = pd.read_csv(self.complete_file_path)
+    self.table.iloc()
 
 def populate_table(self):
     load_table(self)
 
-    print(self.table)
+    print(self.table.iloc[1])
 
-    # for x, column in enumerate(self.table):
-    #     for y, row in enumerate(self.)
+    # for x in range(len(self.table.index)):
+    #     print(self.table.iloc[x][:][:])
     # self.tableWidget.setItem()
+
+def testing():
+    testing_path = r'C:\Users\USER\Documents\GitHub\billing_prediction\src\services\mock\df.csv'
+    testing_df = pd.read_csv(testing_path)
+    print(testing_df.iloc[1].tolist())
+
+
+if __name__ == '__main__':
+    testing()
