8913e35f96505763ad40e4e5aa61d47e88e0c601
JonasFiechter <jonasrfiechter@gmail.com>
2022-08-23 16:27:29 -0300

    .
    .

--- src/features/populate_table.py
+++ src/features/populate_table.py
@@ -1,3 +1,4 @@
+from PyQt5.QtWidgets import *
 import pandas as pd
 
 def load_table(self):
@@ -7,23 +8,23 @@ def load_table(self):
 def populate_table(self):
     load_table(self)
 
-    print(self.table.iloc[1])
+    self.tableWidget.setRowCount(len(self.table.index))
 
-    # for x in range(len(self.table.index)):
-    #     print(self.table.iloc[x][:][:])
-    # self.tableWidget.setItem()
+    for row_index, x in enumerate([self.table.iloc[row_index].tolist() for row_index in range(len(self.table.index))]):
+        for column_index, y in enumerate(x):
+            self.tableWidget.setItem(row_index, column_index, QTableWidgetItem(str(y)))
 
-def testing():
+def module_test():
     testing_path = r'C:\Users\USER\Documents\GitHub\billing_prediction\src\services\mock\df.csv'
     testing_df = pd.read_csv(testing_path)
     
-    # List comprehension to separate all data into a matrix
-    rows = [testing_df.iloc[x].to_list() for x in range(len(testing_df.index))]
-    for x, row in enumerate(rows):
-        print(f'\nx => {x}')
-        for y, column in enumerate(row):
-            print(f'    y => {y} result => {column}')
+    # List comprehension to separate all data into a matrirow_index
+    rows = [testing_df.iloc[row_index].to_list() for row_index in range(len(testing_df.index))]
+    for row_index, row in enumerate(rows):
+        print(f'\nrow_index => {row_index}')
+        for column_index, y in enumerate(row):
+            print(f'    column_index => {column_index} result => {y}')
 
 
 if __name__ == '__main__':
-    testing()
+    module_test()
