30a91be6e48e440e7fdb729104a3c5d8107bfc92
JonasFiechter <jonasrfiechter@gmail.com>
2022-08-26 19:20:20 -0300

    update
    .

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.features.toggle_components import disable_components
 from src.features.predictions import check_prediction
 
 class MainWindow(Ui_MainWindow, QMainWindow):
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__()
         super().setupUi(self)
         
@@ -16,11 +16,31 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         self.btn_calculate.clicked.connect(lambda: check_prediction(self))
         
         # Disable components
-        disable_components(self)
+
+    def disable_components(self):
+        self.btn_calculate.setDisabled(True)
+        self.line_result.setDisabled(True)
+        self.rb_data_segregation.setDisabled(True)
+        self.rb_linear_regression.setDisabled(True)
+        self.rb_mean.setDisabled(True)
+        self.rb_standard_deviation.setDisabled(True)
+        self.rb_time_series.setDisabled(True)
+        self.rb_weighted_average.setDisabled(True)
+
+    def enable_components(self):
+        self.btn_calculate.setDisabled(False)
+        self.line_result.setDisabled(False)
+        self.rb_data_segregation.setDisabled(False)
+        self.rb_linear_regression.setDisabled(False)
+        self.rb_mean.setDisabled(False)
+        self.rb_standard_deviation.setDisabled(False)
+        self.rb_time_series.setDisabled(False)
+        self.rb_weighted_average.setDisabled(False)
         
 
 if __name__ == '__main__':
     qt = QApplication(sys.argv)
     window = MainWindow()
+    window.disable_components()
     window.show()
     qt.exec_()
--- src/features/import_files.py
+++ src/features/import_files.py
@@ -5,27 +5,27 @@ from src.features.toggle_components import enable_components
 # Utils imports
 from src.utils.translate_path import translate
 
-def import_file(self):
+def import_file(window):
     # Open file dialog!
 	try:
 		# The function returns a tuple so let's grab the first index wich is the one we need
-		complete_file_path = QFileDialog.getOpenFileName(self, "Open File", r"", "CSV files (*.csv)")[0]
+		complete_file_path = QFileDialog.getOpenFileName(window, "Open File", r"", "CSV files (*.csv)")[0]
 		file_name = translate(complete_file_path)[0]
 		file_path = translate(complete_file_path)[1]
 
-		# Update self values
-		self.file_name = file_name
-		self.file_path = file_path
-		self.complete_file_path = complete_file_path
+		# Update window values
+		window.file_name = file_name
+		window.file_path = file_path
+		window.complete_file_path = complete_file_path
 
 		# Update screen label with file_name
-		self.lb_file_description.setText(self.file_name)
+		window.lb_file_description.setText(window.file_name)
 
 		# Update tableWidget
-		populate_table(self)
+		populate_table(window)
 
-		# Update components
-		enable_components(self)
+		# Enable components
+		window.enable_components()
 
 	except:
 			print('Import aborted!')
